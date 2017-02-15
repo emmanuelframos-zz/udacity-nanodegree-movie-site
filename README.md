cf312ea4108e322b4788dc8d17730d1efc12e2a6
emmanuelf <emmanuelf@ciandt.com>
2017-02-15 14:32:16 -0200

    Adding comments and re-writing README file

---
+++ README.md
@@ -0,0 +1,22 @@
+# Nano Movie Trailers
+Watch trailers of your favorite movies, and read incredibles storylines. In Nano Movie Trailers, you still can classify your favorite movie as you wish.
+
+## Installation
+You must install Python from [here](https://www.python.org/downloads/).
+
+## Supported Python Versions
+We recommend that you use Python 2.7 or higher.
+
+## Steps to Run Application
+1. Open Python IDLE (Python GUI)
+2. Using Python GUI, open the file: "entertainment_center.py"
+3. Run Module (F5)
+4. Our website will opened
+
+## Usage
+- Click on "Movie Art" (image) to see the Youtube trailer
+- Click on "Storyline" (link) to see the movie storyline
+- We can classify movies clicking on stars
+
+## License
+It is free software, and may be redistributed.
--- media.py
+++ media.py
@@ -1,13 +1,33 @@
-class Video():
+
+class Video:
+    """This class represents a parent class (code reusing) for Video objects, such as: Movies, Series and Documentaries."""
 
     def __init__(self, id, title, storyline, poster_image, youtube_trailer):
+        """
+        Attributes
+        -----------
+        id: Movie's database identificator
+        title: Movie's title
+        storyline: Movie's storyline
+        poster_image: URL movie's of poster
+        youtube_trailer: URL of movie's Youtube trailer
+        """
+
         self.id = id
         self.title = title
         self.storyline = storyline
         self.poster_image = poster_image
         self.youtube_trailer = youtube_trailer
 
+
 class Movie(Video):
+    """This class is a subtype of Video, and inherit attributes from parent class."""
 
     def __init__(self, id, title, storyline, poster_image, youtube_trailer):
-        Video.__init__(self, id, title, storyline, poster_image, youtube_trailer)
+        Video.__init__(
+            self,
+            id,
+            title,
+            storyline,
+            poster_image,
+            youtube_trailer)
